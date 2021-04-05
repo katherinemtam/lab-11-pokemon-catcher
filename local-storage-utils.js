bedfcbe413312c308dab44e1f38e6efbb7441d56
Katherine Tam <katherinemtam@gmail.com>
2021-04-05 15:25:23 -0700

    add getPokdex, setPokedex, findById functions

---
+++ local-storage-utils.js
@@ -0,0 +1,13 @@
+const POKEDEX = 'POKEDEX';
+
+export function getPokedex() {
+    const stringyPokedex = localStorage.getItem(POKEDEX);
+    if (!stringyPokedex) return [];
+    const parsedPokedex = JSON.parse(stringyPokedex);
+    return parsedPokedex;
+}
+
+export function setPokedex(parsedPokedex) {
+    const stringyPokedex = JSON.stringify(parsedPokedex);
+    localStorage.setItem(stringyPokedex);
+}

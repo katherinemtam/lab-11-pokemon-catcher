d7d7a9f6477986276fe4c98f30fddeb67349ea57
Katherine Tam <katherinemtam@gmail.com>
2021-04-05 23:30:20 -0700

    add results display

---
+++ results/app.js
@@ -0,0 +1,15 @@
+import { getPokedex } from '../local-storage-utils.js';
+import { createTableRow } from './utils.js';
+
+const playAgain = document.querySelector('#play-again-button');
+const table = document.querySelector('#table');
+const pokedex = getPokedex();
+
+playAgain.addEventListener('click', () => {
+    window.location = '../index.html';
+});
+
+for (let pokemon of pokedex) {
+    const tr = createTableRow(pokemon);
+    table.append(tr);
+}
